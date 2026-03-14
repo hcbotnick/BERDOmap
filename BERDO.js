9e2a5ed70013775867038f948645cb2c9b5ec902
hcbotnick <botnick.h@northeastern.edu>
2026-03-14 17:21:52 -0400

    Restore config object in BERDO.js

--- BERDO.js
+++ BERDO.js
@@ -184,7 +184,7 @@ var config = {
                 center: [-71.10593, 42.33676],
                 zoom: 16.76,
                 pitch: 85,
-                bearing: -162.42
+                bearing: 114.96
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
